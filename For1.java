0fa924f1881db49db993a0175e1ac7e3d1e20636
José Gabriel <64782767+gab-rod@users.noreply.github.com>
2021-10-13 00:51:51 -0300

    Add files via upload

---
+++ For1.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class For1 {
+  
+	public static void main(String[] args) {
+
+// Este programa tem a função de demonstrar a utilização de um for. O contador é disparado somando um número por vez e chegará até 10.
+		
+		for (int contador = 1; contador <= 10; contador++) {
+		System.out.printf("i = %d\n", contador);
+		
+		}
+}
+} 
---
+++ For2.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		
+//Neste caso o contador irá reduzir de 2 em 2 números. Até chegar no número 0.
+		
+		for (int contador = 10; contador >= 0; contador -=2) {
+			System.out.printf("a = %d\n", contador);
+		}
+		
+	}
+} 
